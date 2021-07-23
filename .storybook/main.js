295eaa506ca68f97371c0d71c54d8bcf05a337e9
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-07-22 16:35:24 -1000

    chore: add working Storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,27 @@
+const path = require("path");
+
 // const { getStoriesPaths } = require('slice-machine-ui/helpers/storybook')
 
 module.exports = {
-	stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-	addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+	core: {
+		builder: "webpack5",
+	},
+	stories: [
+		"../src/**/*.stories.mdx",
+		"../src/**/*.stories.@(js|jsx|ts|tsx)",
+		"../.slicemachine/**/*.stories.js",
+	],
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		{
+			name: "@storybook/addon-postcss",
+			options: {
+				postcssLoaderOptions: {
+					implementation: require("postcss"),
+				},
+			},
+		},
+		path.resolve("./.storybook/addon-gatsby.js"),
+	],
 };
-
